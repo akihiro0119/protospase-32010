935e5a9924438303b919b7dc861f7c1b03f46c88
akihiro0119 <aono1x19@gmail.com>
2020-11-14 00:41:18 +0900

    画像投稿機能、バリデーションの設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,9 @@
 class Prototype < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  vilidates :catch_copy, presence: true
+  validates :consept, presence: true
+  validates :image, presence: true
 end
