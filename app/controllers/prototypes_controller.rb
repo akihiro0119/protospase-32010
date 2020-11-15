fd9e6c2b6af92b35a76198897e3c6f75d87ae557
akihiro0119 <aono1x19@gmail.com>
2020-11-15 17:36:13 +0900

    提出前

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show] 
+  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -12,6 +13,8 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def create
@@ -27,13 +30,21 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
-    @prototype.update(prototype_params)
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
       if @prototype.save
         redirect_to root_path
       else
         render :edit
       end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+    
+
   end
 
   private
@@ -43,8 +54,10 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user.id
       redirect_to action: :index
     end
   end
+
 end
